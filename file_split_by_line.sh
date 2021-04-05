bd6132c15b90424c5ce5e22b9bdfb8843d29d728
Neeka Sewnath <nsewnath@ufl.edu>
2021-04-05 10:22:43 -0400

    fixed typos

--- file_split_by_line.sh
+++ file_split_by_line.sh
@@ -1,6 +1,6 @@
 #Quickly split lines of a file into multiple, numbered files with bash loop
 #Neeka Sewnath
-#nsewnath@bear.flmnh.ufl.edu
+#nsewnath@ufl.edu
 
 ###Fetch Samples###
 
@@ -9,7 +9,6 @@
 # In the third part of the pipe, use cut to trim down sample name
 # In the fourth part of the pipe, replace "_" with desired delimiter 
 
-
 for h in `ls *.ml | sed 's/\./\t/g' | cut -f1 | sort | uniq`; do
 
 echo ${h}
@@ -17,7 +16,7 @@ echo ${h}
 # initiate counter
 counter=1
 
-# Reach fiel and  assign every line of file to new file
+# Reach files and assign every line of file to new file
 while read LINE; do echo $LINE > ${h}_${counter}.ml; counter=$((counter+1)); done < ${h}.ml
 
 done
